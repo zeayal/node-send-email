c6aed5c0d70b7002fefae67ebee39f8a1cfd9bb4
ziyang.liu <ziyang.liu@mobilemd.cn>
2021-07-14 12:07:54 +0800

    init projec

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const schedule = require("node-schedule");
+const axios = require("axios");
+const dotenv = require('dotenv');
+const { sendEmail } = require("./service/email");
+
+// 加载环境变量
+const envConfig = dotenv.config()
+if (envConfig.error) {
+  throw envConfig.error
+}
+
+for(const key in envConfig.parsed) {
+    process.env[key] = envConfig.parsed[key];
+}
+
+console.log(envConfig.parsed)
+
+// 定时执行规则 https://segmentfault.com/a/1190000022455361
+let rule = new schedule.RecurrenceRule();
+// 每小时30分执行
+rule.minute = 11;
+rule.second = 0;
+// rule.second = [0, 10, 20, 30, 40, 50]; // 每隔 10 秒执行一次
+
+const job = schedule.scheduleJob(rule, () => {
+  getCoinPrirce({
+    coinId: 'dogecoin',
+    expectedPrice: 0.2
+  });
+});
+
+async function getCoinPrirce(
+  { coinId, expectedPrice } = { coinId: "dogecoin", expectedPrice: 0.3 }
+) {
+  console.log(coinId, expectedPrice);
+  const res = await axios.get(
+    "https://fxhapi.feixiaohao.com/public/v1/ticker?limit=30"
+  );
+  const coins = res.data;
+  if (Array.isArray(coins)) {
+    const selectedCoin = coins.find((item) => item.id === coinId);
+    if (selectedCoin && selectedCoin.price_usd >= expectedPrice) {
+      // 发送邮件
+      sendEmail({
+        from: process.env.EMAIL_FORM,
+        to: process.env.EMAIL_TO.split(','),
+        subject: "gmail提示",
+        text: `${coinId}-价格变动`,
+        html: `<b>${coinId}-当前价格$${selectedCoin.price_usd}</b>`,
+      }).catch(console.error);
+    }
+  }
+}
---
+++ src/service/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+async function sendEmail(mailOptions = {
+    from: 'Sender Name <sender@server.com>',
+    to: 'admin@admin.com',
+    subject: "hello subject",
+    text: 'hello text',
+    html: "<b>Hello world html</b>"
+}) {
+    // let testAccount = await nodemailer.createTestAccount();
+    // create reusable transporter object using the default transport
+    let transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_SMTP_HOST,
+        port: process.env.EMAIL_SMTP_PORT,
+        secure: process.env.EMAIL_SMTP_SECURE,
+        auth: {
+            user: process.env.EMAIL_SMTP_AUTH_USERNAME,
+            pass: process.env.EMAIL_SMTP_AUTH_PASSWORD
+        }
+    })
+
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log('message sent: s%', info.messageId);
+    console.log('preview URL: %s', nodemailer.getTestMessageUrl(info))
+}
+
+exports.sendEmail = sendEmail
